debbc6480f5acb02b539ff6cd126e57eb579d5c6
Eliyahu Levy <eliyahu.levy21@gmail.com>
2023-11-08 17:07:40 -0500

    Cleanup and better person

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,20 @@
 import P5, { Vector } from "p5";
-import Tile from "./Tile";
 import MyVector from "./MyVector";
 import Chunkloader from "./Chunkloader";
 import WorldGenerator from "./WorldGenerator";
 
 new P5((p5: P5) => {
     const scl: number = 100;
-    const worldOffset: MyVector = new MyVector(0, 0);
-
-    const moving: MyVector = new MyVector(0, 0);
-
     const moveSpeed = 9;
+
+    const worldOffset: MyVector = new MyVector(0, 0);
+    const playerChunk = new MyVector(0, 0);
+    const playerPos = new MyVector(0, 0);
+    const moving: MyVector = new MyVector(0, 0);
 
     let chunkLoader: Chunkloader;
     let worldGenerator: WorldGenerator;
 
-    const playerChunk = new MyVector(0, 0);
-    const playerPos = new MyVector(0, 0);
 
     p5.setup = () => {
         const canvas = p5.createCanvas(p5.windowWidth - 10, p5.windowHeight - 10);
@@ -26,7 +24,14 @@ new P5((p5: P5) => {
         p5.frameRate(30);
 
         worldGenerator = new WorldGenerator(scl, scl / 20);
-        chunkLoader = new Chunkloader(p5, 8, 5, scl, worldGenerator);
+
+        let xChunks = Math.floor(p5.windowWidth / scl);
+        let yChunks = Math.floor(p5.windowHeight / scl);
+        console.log(xChunks, yChunks)
+
+        chunkLoader = new Chunkloader(p5, xChunks, yChunks, scl, worldGenerator);
+
+        console.log(chunkLoader.tileLookupTable.get(-1).get(0))
     };
 
     p5.draw = () => {
@@ -39,28 +44,49 @@ new P5((p5: P5) => {
         
         if (chunkLoader.canMove(playerPos.x + moving.x * moveSpeed, playerPos.y)) {
             worldOffset.x += -moving.x * moveSpeed;
-        } else {
-            console.log("Cant move x");
         }
 
         if (chunkLoader.canMove(playerPos.x , playerPos.y + moving.y * moveSpeed)) {
             worldOffset.y += -moving.y * moveSpeed;
-        }  else {
-            
-            console.log("Cant move y");
-        }
+        } 
         p5.push();
         p5.translate(worldOffset.x, worldOffset.y);
         chunkLoader.displayLoadedChunks(p5, playerChunk.x, playerChunk.y);
         p5.pop();
 
-        p5.fill("cyan");
-        p5.ellipse(p5.width / 2, p5.height / 2, scl * 0.4);
+        drawPlayer(p5);
+
+
+        
 
         chunkLoader.updateLoadedChunks(p5, playerChunk.x, playerChunk.y);
 
         displayStats(p5);
     };
+
+    function drawPlayer(p5: P5) {
+        p5.push();
+        p5.translate(p5.width / 2, p5.height / 2);
+        p5.rectMode(p5.CENTER);
+        p5.fill("lightgreen");
+        p5.rect(0, 0, scl / 5, scl/5, scl/30);
+        // Left eye
+        p5.fill(0);
+        p5.ellipse(-scl / 20, -scl / 25, scl / 13);
+        p5.fill(255);
+        p5.ellipse(-scl / 20, -scl / 25, scl / 25);
+
+        // Right eye        
+        p5.fill(0);
+        p5.ellipse(scl / 20, -scl / 25, scl / 13);
+        p5.fill(255);
+        p5.ellipse(scl / 20, -scl / 25, scl / 25);
+
+        p5.fill(0)
+        p5.arc(0, scl/30, scl/7, scl/13, 0, p5.PI);
+
+        p5.pop();
+    }
 
     function displayStats(p5: P5) {
         p5.textSize(20);
